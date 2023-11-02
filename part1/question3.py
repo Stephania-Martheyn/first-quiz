6ff7f8b94506e841c8f86e3290b0182cc850dab3
Stephania-Martheyn <stephaniemartheyn@hotmail.com>
2023-11-01 22:07:07 -0500

    Se realizo el ejercicio question3.py

--- part1/question3.py
+++ part1/question3.py
@@ -26,19 +26,82 @@
 # formulas and their outputs in the test file, `question3_test.py`.
 
 # This function should return an oven instance!
+
+from abc import ABC, abstractmethod
+
+class CookingError(Exception):
+    pass
+
+class IngredientError(Exception):
+    pass
+
+class Cookable(ABC):
+    @abstractmethod
+    def add(self, item):
+        pass
+
+    @abstractmethod
+    def cook(self, temperature):
+        pass
+
+    @abstractmethod
+    def get_output(self):
+        pass
+
+class Oven(Cookable):
+    def __init__(self):
+        self.ingredients = []
+
+    def add(self, ingredients):
+        self.ingredients = ingredients
+
+    def cook(self, temperature):
+        if temperature < 0:
+            result = self.freeze()
+        elif temperature >= 100:
+            result = self.boil()
+        else:
+            self.wait()
+            result = None
+
+        if result is not None:
+            self.ingredients = [result]
+
+    def freeze(self):
+        if "water" in self.ingredients and "air" in self.ingredients:
+            return "snow"
+
+    def boil(self):
+        if "lead" in self.ingredients and "mercury" in self.ingredients:
+            return "gold"
+        elif "cheese" in self.ingredients and "dough" in self.ingredients and "tomato" in self.ingredients:
+            return "pizza"
+
+    def wait(self):
+        pass
+
+    def get_output(self):
+        if self.ingredients:
+            return " and ".join(self.ingredients)
+        else:
+            return ""
+
 def make_oven():
-  None
+    return Oven()
 
 def alchemy_combine(oven, ingredients, temperature):
-  
-  for item in ingredients:
-    oven.add(item)
+    if not isinstance(oven, Oven):
+        raise CookingError("Se esperaba una instancia de Oven para cocinar.")
 
-  if temperature < 0:
-    oven.freeze()
-  elif temperature >= 100:
-    oven.boil()
-  else:
-    oven.wait()
+    try:
+        oven.add(ingredients)
+        oven.cook(temperature)
+        return oven.get_output()
+    except CookingError as ce:
+        print(f"Error de cocción: {ce}")
+    except IngredientError as ie:
+        print(f"Error de ingrediente: {ie}")
+    except Exception as e:
+        print(f"Error inesperado: {e}")
 
-  return oven.get_output()
+
--- part1/question3_test.py
+++ part1/question3_test.py
@@ -1,5 +1,7 @@
-def test_alchemy_combine():
 
+from question3 import alchemy_combine, make_oven
+
+def test_alchemy_combine():
   assert alchemy_combine(
     make_oven(),
     ["lead", "mercury"],
