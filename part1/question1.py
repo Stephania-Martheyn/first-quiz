9a67d0e6a05e0d0e625547c7d075d541a3d7f350
Stephania-Martheyn <stephaniemartheyn@hotmail.com>
2023-11-01 20:01:22 -0500

    se realiza question1_test.py y se realizan correciones en question1.py

--- part1/question1.py
+++ part1/question1.py
@@ -13,23 +13,37 @@
 # need to be fixed. The test suite in `question1_test.py` will verify the output.
 # Read the test suite to know the values that these functions should return.
 
-def get_city_temperature(city):
-   if city == "Quito":
-      return 22
-   if city == "Sao Paulo":
-      return 17
-   if city == "San Francisco":
-      return 16
-
-def get_city_weather(city):
-
-  sky_condition = None
-
-  if city == "Sao Paulo":
-     sky_condition = "cloudy"
-  elif city == "New York":
-     sky_condition = "rainy"
-
-  temperature = get_city_temperature(city)
-
-  return str(temperature) + " degrees and " + sky_condition
+def get_city_temperature(city): 
+    temperatures =  { 
+        "Quito": 22, 
+        "Sao Paulo": 17, 
+        "San Francisco": 16, 
+        "New York": 14 
+     } 
+    try: 
+        return temperatures[city] 
+    except KeyError: 
+        raise KeyError(f"The city ' {city }' does not have a registered temperature.") 
+ 
+ 
+ 
+ 
+def get_city_weather(city): 
+    conditions =  { 
+        "Sao Paulo": "cloudy", 
+        "New York": "rainy", 
+        "Quito": "sunny", 
+        "San Francisco":"cloudy"
+     } 
+ 
+    try: 
+        sky_condition = conditions[city] 
+    except KeyError: 
+        raise KeyError(f"The city ' {city }' does not have registered weather conditions.") 
+ 
+    try: 
+        temperature = get_city_temperature(city) 
+    except KeyError: 
+        raise KeyError(f"The temperature for the city ' {city }' could not be found.") 
+ 
+    return f"{temperature } degrees and {sky_condition }" 
--- part1/question1_test.py
+++ part1/question1_test.py
@@ -1,7 +1,13 @@
-from question1 import get_city_weather
-
-def test_get_city_weather():
-
-  assert get_city_weather("Quito") == "22 degrees and sunny"
-
+from question1 import get_city_weather 
+ 
+def test_get_city_weather(): 
+ 
+  assert get_city_weather("Quito") == "22 degrees and sunny" 
+ 
   assert get_city_weather("New York") == "14 degrees and rainy"
+
+  assert get_city_weather("Sao Paulo") == "17 degrees and cloudy" 
+
+  assert get_city_weather("San Francisco") == "16 degrees and cloudy"
+ 
+
